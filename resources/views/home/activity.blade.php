30efbbb04e1286169dcd78f8a87e2c0efd248536
WJK430821 <13681333005@163.com>
2017-07-03 13:56:14 +0800

    aa

---
+++ resources/views/home/activity.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<!--<![endif]--><head>
+  <title>热映口碑榜 - 猫眼电影 - 一网打尽好电影</title>
+  <meta charset="UTF-8">
+  <meta name="keywords" content="猫眼电影,电影排行榜,热映口碑榜,最受期待榜,国内票房榜,北美票房榜,猫眼TOP100">
+  <meta name="description" content="猫眼电影热门榜单,包括热映口碑榜,最受期待榜,国内票房榜,北美票房榜,猫眼TOP100,多维度为用户进行选片决策">
+  <link rel="stylesheet" href="css/common.dce64fb8.css">
+  <link rel="stylesheet" href="css/board-index.92a06072.css">
+  
+</head>
+<body>
+<div class="header">
+  <div class="header-inner">
+        <a href="http://maoyan.com/" class="logo" data-act="icon-click"></a>
+        <div class="city-container" data-val="{currentcityid:1 }">
+            <div class="city-selected">
+                <div class="city-name">
+                  北京
+                  <span class="caret"></span>
+                </div>
+            </div>
+            <div class="city-list" data-val="{ localcityid: 1 }">
+                <div class="city-list-header">定位城市：<a class="js-geo-city">北京</a></div> 
+            </div>
+        </div>
+
+
+        <div class="nav">
+            <ul class="navbar">
+                <li><a href="http://maoyan.com/" data-act="home-click">首页</a></li>
+                
+                <li><a href="http://maoyan.com/films" data-act="movies-click">电影</a></li>
+                
+                <li><a href="http://maoyan.com/board" data-act="board-click" class="active">榜单</a></li>
+                <li><a href="http://maoyan.com/news" data-act="hotNews-click">热点</a></li>
+            </ul>
+        </div>
+
+        <div class="user-info">
+            <div class="user-avatar J-login">
+              <img src="7dd82a16316ab32c8359debdb04396ef2897.png">
+              <span class="caret"></span>
+              <ul class="user-menu">
+                <li><a href="javascript:void 0">登录</a></li>
+                <li><a href="javascript:void 0">注册</a></li>
+              </ul>
+            </div>
+        </div>
+
+        <form action="http://maoyan.com/query" target="_blank" class="search-form" data-actform="search-click">
+            <input name="kw" class="search" maxlength="32" placeholder="找影视剧、影人、影院" autocomplete="off" type="search">
+            <input class="submit" value="" type="submit">
+        </form>
+
+  </div>
+</div>
+<div class="header-placeholder"></div>
+    <div class="container" id="app">
+
+<div class="content">
+    <div class="wrapper">
+        <div class="main">
+            <p class="update-time"><img src="images/active.png"></p>
+            <p class="board-content">榜单规则：将昨日国内热映的影片，按照评分从高到低排列取前10名，每天上午10点更新。相关数据来源于“猫眼专业版”及“猫眼电影库”。</p>
+            <dl class="board-wrapper">
+                <dd>
+                    <i class="board-index board-index-1">1</i>
+                      <a href="http://maoyan.com/films/588362" title="摔跤吧！爸爸" class="image-link" data-act="boarditem-click" data-val="{movieId:588362}">
+                      <img src="loading_2.e3d934bf.png" alt="" class="poster-default">
+                      <img alt="摔跤吧！爸爸" class="board-img" src="aeb864fa21d578d845b9cefc056e40cb2874891.jpg@160w_220h_1e_1c">
+                    </a>
+                    <div class="board-item-main">
+                      <div class="board-item-content">
+                          <div class="movie-item-info">
+                          <p class="name"><a href="http://maoyan.com/films/588362" title="摔跤吧！爸爸" data-act="boarditem-click" data-val="{movieId:588362}">摔跤吧！爸爸</a></p>
+                          <p class="star">
+                                  主演：阿米尔·汗,萨卡诗·泰瓦,法缇玛·萨那·纱卡
+                          </p>
+                          <p class="releasetime">上映时间：2017-05-05</p>    </div>
+                          <div class="movie-item-number score-num">
+                            <p class="score"><i class="integer">9.</i><i class="fraction">8</i></p>        
+                          </div>
+
+                      </div>
+                    </div>
+                </dd>
+               
+            </dl>
+
+        </div>
+    </div>
+</div>
+
+    </div>
+<div class="footer" style="visibility: visible;">
+    <p class="friendly-links">
+        友情链接 :
+        <a href="http://www.meituan.com/" data-query="utm_source=wwwmaoyan" target="_blank">美团网</a>
+        <span></span>
+        <a href="http://i.meituan.com/client" data-query="utm_source=wwwmaoyan" target="_blank">美团下载</a>
+    </p>
+    <p>
+        ©2016
+        猫眼电影 maoyan.com
+        <a href="https://tsm.miit.gov.cn/pages/EnterpriseSearchList_Portal.aspx?type=0&amp;keyword=%E4%BA%ACICP%E8%AF%81160733%E5%8F%B7&amp;pageNo=1" target="_blank">京ICP证160733号</a>
+        <a href="http://www.miibeian.gov.cn/" target="_blank">京ICP备16022489号-1</a>
+        京公网安备 11010502030881号
+        <a href="http://maoyan.com/about/licence" target="_blank">网络文化经营许可证</a>
+        <a href="http://www.meituan.com/about/rules" target="_blank">电子公告服务规则</a>
+    </p>
+    <p>北京猫眼文化传媒有限公司</p>
+</div>
+    <!--[if IE 8]><script src="//ms0.meituan.net/mywww/es5-shim.bbad933f.js"></script><![endif]-->
+    <!--[if IE 8]><script src="//ms0.meituan.net/mywww/es5-sham.d6ea26f4.js"></script><![endif]-->
+    <script src="common.78ef724e.js"></script>
+<script src="board-index.54b406df.js"></script>
+
+
+</body>
+</html>
---
+++ resources/views/home/cinema.blade.php
@@ -0,0 +1,107 @@
+@extends('home.base')
+  @section('content')
+	<!-- 影院css -->
+	<link rel="stylesheet" href="{{asset('home/css/global-min.css')}}">
+  	<link rel="stylesheet" href="{{asset('home/css/_001.css')}}" clam-moveto="none">
+  	<link rel="stylesheet" href="{{asset('home/css/.css')}}">
+	<div class="cinema-wrap center-wrap">
+		<div class="filter-wrap" data-spm="w2">
+			<ul class="filter-select">
+				<li>
+					<label>选择区域</label>
+					<div class="select-tags">
+						<a class="current" href="http://dianying.taobao.com/cinemaList.htm?regionName=&amp;n_s=new" data-param="city=110100">全部区域</a>
+						<a href="http://dianying.taobao.com/cinemaList.htm?regionName=%E6%98%8C%E5%B9%B3%E5%8C%BA&amp;n_s=new">昌平区</a>
+						<a href="http://dianying.taobao.com/cinemaList.htm?regionName=%E6%9C%9D%E9%98%B3%E5%8C%BA&amp;n_s=new">朝阳区</a>
+					</div>					
+					<a class="J_show select-show" href="javascript:;">更多&gt;</a></li>
+				
+			
+			</ul>
+			<div class="list-sort M-sort">
+				排序：<a class="active" href="http://dianying.taobao.com/cinemaList.htm?regionName=&amp;sortType=0&amp;cinemaName=&amp;activityId=&amp;fCode=&amp;n_s=new">综合</a> 
+				当前条件下共有<span class="count">177</span>家影院 
+			</div>
+			<ul class="sortbar-detail J_cinemaList">
+			    <li class="current">
+					<div class="detail-right">
+						<div class="right-score">评分：<strong>9.4</strong></div>
+						<div class="right-fav "></div>
+						<div class="right-buy ">    							
+							<a href="/">选座</a>
+						</div>
+					</div>
+					<a href="/cinemas/id=1" class="detail-left pictures">
+						  <span><img src="T1yI5PFuhdXXXXXXXX_.jpg_120x120.jpg" alt="卢米埃北京芳草地影城"></span>								
+					</a>
+					<div class="detail-middle">
+						<div class="middle-hd">
+							<h4><a href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=9462&amp;n_s=new">卢米埃北京芳草地影城</a></h4>
+						</div>
+						<div class="middle-p">
+							<div class="middle-p-list"><i>地址：</i><span class="limit-address">朝阳区东大桥路9号芳草地大厦LG2-26</span><a class="J_miniMap" href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=9462&amp;n_s=new#detail" data-points="116.449826,39.919551">[地图]</a></div>    							<div class="middle-p-list"><i>电话：</i>010-56907679,0</div>    							<div class="middle-p-list"><i>更多：</i><a class="middle-more" href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=9462&amp;n_s=new#detail">影院服务</a><a class="middle-more" href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=9462&amp;n_s=new#detail">交通信息</a></div>
+						</div>
+					</div>
+				</li>
+			    <li class="even">
+					<div class="detail-right">
+						<div class="right-score">评分：<strong>9.1</strong></div>
+						<div class="right-fav "></div>
+						<div class="right-buy ">  										<a href="">选座</a>
+						</div>
+					</div>
+					<a href="" class="detail-left pictures">
+						  <span><img src="T1XfCPFqBkXXb1upjX_120x120.jpg" alt="橙天嘉禾影城北京凤凰汇店"></span>
+													
+					</a>
+					<div class="detail-middle">
+						<div class="middle-hd">
+							<h4><a href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=6364&amp;n_s=new">橙天嘉禾影城北京凤凰汇店</a></h4>
+						</div>
+						<div class="middle-p">
+							<div class="middle-p-list"><i>地址：</i><span class="limit-address">朝阳区三元桥曙光西里甲5号院凤凰汇购物中心3层</span><a class="J_miniMap" href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=6364&amp;n_s=new#detail" data-points="116.457572,39.963406">[地图]</a></div>    							<div class="middle-p-list"><i>电话：</i>010-56383227,8001</div>    							<div class="middle-p-list"><i>更多：</i><a class="middle-more" href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=6364&amp;n_s=new#detail">影院服务</a><a class="middle-more" href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=6364&amp;n_s=new#detail">交通信息</a></div>
+						</div>
+					</div>
+				</li>
+				<li class="current">
+					<div class="detail-right">
+						<div class="right-score">评分：<strong>9.1</strong></div>
+						<div class="right-fav "></div>
+						<div class="right-buy ">  										<a href="">选座</a>
+						</div>
+					</div>
+					<a href="" class="detail-left pictures">
+						  <span><img src="T1XfCPFqBkXXb1upjX_120x120.jpg" alt="橙天嘉禾影城北京凤凰汇店"></span>
+													
+					</a>
+					<div class="detail-middle">
+						<div class="middle-hd">
+							<h4><a href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=6364&amp;n_s=new">橙天嘉禾影城北京凤凰汇店</a></h4>
+						</div>
+						<div class="middle-p">
+							<div class="middle-p-list"><i>地址：</i><span class="limit-address">朝阳区三元桥曙光西里甲5号院凤凰汇购物中心3层</span><a class="J_miniMap" href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=6364&amp;n_s=new#detail" data-points="116.457572,39.963406">[地图]</a></div>    							<div class="middle-p-list"><i>电话：</i>010-56383227,8001</div>    							<div class="middle-p-list"><i>更多：</i><a class="middle-more" href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=6364&amp;n_s=new#detail">影院服务</a><a class="middle-more" href="http://dianying.taobao.com/cinemaDetail.htm?cinemaId=6364&amp;n_s=new#detail">交通信息</a></div>
+						</div>
+					</div>
+				</li>
+			</ul>
+					
+			<div class="sortbar-more J_cinemaMore" data-ajax="http://dianying.taobao.com/ajaxCinemaList.htm" data-param="page=1&amp;regionName=&amp;cinemaName=&amp;pageSize=10&amp;pageLength=18&amp;sortType=0&amp;n_s=new">
+                <div>
+                   <a href="javascript:;">点击显示更多</a>
+                </div>
+            </div>
+						
+		</div>
+
+		<div class="Coupon-list" data-spm="w3">
+			<div class="list-hd ">
+				<h4>热销团购券</h4>
+				<a href="http://dianying.taobao.com/itemList.htm?n_s=new}">查看全部&nbsp;&gt;</a>
+			</div>
+			<div class="error-wrap"><div class="title">暂无团购信息</div>你可以查看 <a href="">全部影院</a>或者<a href="http://dianying.taobao.com/showList.htm?n_s=new">全部影片</a></div>
+						
+		</div>
+		<div class="cinema-sortbar">
+		</div>
+	</div>
+  @endsection
