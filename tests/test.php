6293e71a41904ce881d2eef20c731a25258c88ce
WJK430821 <13681333005@163.com>
2017-07-03 13:27:17 +0800

    瞎测试

--- tests/test.php
+++ tests/test.php
@@ -4,7 +4,7 @@
 
 
 	echo  'changtianfei test';
-<<<<<<< HEAD
+
 	echo  'changtianfei test';
 	echo  'changtianfei test';
 	echo  'changtianfei test';
@@ -19,6 +19,3 @@
 	echo  'changtianfei test';
 	echo  'changtianfei test';
 		
-
-=======
->>>>>>> 62af93b94a5015f2aa96fc167e548101f56054d5
