c3ef9023978b36275b8bf4eb227cf7ed2ae27c07
sangdaheng <yy806504112@qq.com>
2017-07-13 10:45:36 +0800

    合并一二分支

--- resources/views/home/layout.blade.php
+++ resources/views/home/layout.blade.php
@@ -1,76 +1,80 @@
 @extends('home.base')
- @section('content')
-<link rel="stylesheet" type="text/css" href="{{asset('home/css/main.css')}}" />
-<style type="text/css">
-.demo{width:900px; margin:40px auto 0 auto; min-height:450px;}
-@media screen and (max-width: 360px) {.demo {width:340px}}
+@section('content')
+	<link rel="stylesheet" type="text/css" href="{{asset('home/css/main.css')}}" />
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<style type="text/css">
+		.demo{width:700px; margin:40px auto 0 auto; min-height:450px;}
+		@media screen and (max-width: 360px) {.demo {width:340px}}
 
-.front{width: 300px;margin: 5px 32px 45px 32px;background-color: #f0f0f0;	color: #666;text-align: center;padding: 3px;border-radius: 5px;}
-.booking-details {float: right;position: relative;width:220px;height: 450px; }
-.booking-details h3 {margin: 5px 5px 0 0;font-size: 16px;}
-.booking-details p{line-height:26px; font-size:16px; color:#999}
-.booking-details p span{color:#666}
-div.seatCharts-cell {color: #182C4E;height: 25px;width: 26px;line-height: 25px;margin: 3px;float: left;text-align: center;outline: none;font-size: 13px;}
-div.seatCharts-seat {cursor: pointer;-webkit-border-radius: 5px;-moz-border-radius: 5px;border-radius: 5px;}
-div.seatCharts-row {height: 35px;}
-.seatbutton{background:{{url(asset('home/images/26-25.png'))}};}
-div.seatCharts-seat.available {background:{{url(asset('home/images/26-25.png'))}};}
-div.seatCharts-seat.available1{background:{{url(asset('home/images/hong26-25.png'))}};}
-div.seatCharts-seat.focused {background-color: #76B474;border: none;}
-div.seatCharts-seat.selected {background:{{url(asset('home/images/hong26-25.png'))}};}
-div.seatCharts-seat.unavailable {background:{{url(asset('home/images/26-25hei.png'))}};cursor: not-allowed;}
+		.front{width: 300px;margin: 5px 32px 45px 32px;background-color: #f0f0f0;	color: #666;text-align: center;padding: 3px;border-radius: 5px;}
+		.booking-details {float: right;position: relative;width:220px;height: 450px; }
+		.booking-details h3 {margin: 5px 5px 0 0;font-size: 16px;}
+		.booking-details p{line-height:26px; font-size:16px; color:#999}
+		.booking-details p span{color:#666}
+		div.seatCharts-cell {color: #182C4E;height: 25px;width: 26px;line-height: 25px;margin: 3px;float: left;text-align: center;outline: none;font-size: 13px;}
+		div.seatCharts-seat {cursor: pointer;-webkit-border-radius: 5px;-moz-border-radius: 5px;border-radius: 5px;}
+		div.seatCharts-row {height: 35px;}
 
-div.seatCharts-container {border-right: 1px dotted #adadad;width: 400px;padding: 20px;float: left;}
-div.seatCharts-legend {padding-left: 0px;position: absolute;bottom: 16px;}
-ul.seatCharts-legendList {padding-left: 0px;}
-.seatCharts-legendItem{float:left; width:90px;margin-top: 10px;line-height: 2;}
-span.seatCharts-legendDescription {margin-left: 5px;line-height: 30px;}
-.checkout-button {display: block;
-    text-align: center;
-    margin-top: 20px;
-    cursor: pointer;
-    background: rgb(235, 0, 42) none repeat scroll 0% 0%;
-    border: 0px none;
-    width: 155px;
-    height: 42px;
-    line-height: 42px;
-    color: rgb(255, 255, 255);
-    font-size: 14px; cursor:pointer}
-#selected-seats {max-height: 150px;overflow-y: auto;overflow-x:none;width: 200px;}
-#selected-seats li{float:left; width:72px; height:26px; line-height:26px; border:1px solid #d3d3d3; background:#f7f7f7; margin:6px; font-size:14px; font-weight:bold; text-align:center}
-</style>
+		div.seatCharts-seat.available {background:{{url(asset('home/images/26-25.png'))}};}
+		div.seatCharts-seat.available1{background:{{url(asset('home/images/hong26-25.png'))}};}
+		div.seatCharts-seat.focused {background-color: #76B474;border: none;}
+		div.seatCharts-seat.selected {background:{{url(asset('home/images/hong26-25.png'))}};}
+		div.seatCharts-seat.unavailable {background:{{url(asset('home/images/26-25hei.png'))}};cursor: not-allowed;}
+		div.seatCharts-container {border-right: 1px dotted #adadad;width: 400px;padding: 20px;float: left;}
+		div.seatCharts-legend {padding-left: 0px;position: absolute;bottom: 16px;}
+		ul.seatCharts-legendList {padding-left: 0px;}
+		.seatCharts-legendItem{float:left; width:90px;margin-top: 10px;line-height: 2;}
+		span.seatCharts-legendDescription {margin-left: 5px;line-height: 30px;}
+		.checkout-button {display: block;
+			text-align: center;
+			margin-top: 20px;
+			cursor: pointer;
+			background: rgb(235, 0, 42) none repeat scroll 0% 0%;
+			border: 0px none;
+			width: 155px;
+			height: 42px;
+			line-height: 42px;
+			color: rgb(255, 255, 255);
+			font-size: 14px; cursor:pointer}
+		#selected-seats {max-height: 150px;overflow-y: auto;overflow-x: none;width: 200px;}
+		#selected-seats li{float:left; width:72px; height:26px; line-height:26px; border:1px solid #d3d3d3; background:#f7f7f7; margin:6px; font-size:14px; font-weight:bold; text-align:center}
+	</style>
 
+	</head>
 
-<div id="main">
-   <h2 class="top_title">{{$ctit->shopname}}</h2>
-   <div class="demo">
-   		<center>
-	   <div id="seat-map" style="width:470px;float:left;">
-			<div class="front">屏幕中央</div>
-			<div id="newseatmap">
-				@foreach ($layout as $key=>$value)
-				@endforeach
-				@for ($i = 0; $i <=$key; $i++)
-					<ul value="{{($i)}}">
-						@php
-						{{-- @todo 处理一个数组将座位信息删除  显示真实的座位信息 --}}
-						@endphp
-						@for($j=0;$j<count($value);$j++)
-							@if($value[$j]=='a')
-								<button class="seatbutton" issale = "{{$value[$j]}}" cowsnumber="{{$i+1}}" value="">{{$j+1}}</button>
-							@elseif ($value[$j]=='_')
-								<button style="visibility:hidden" value="{{$value[$j]}}">{{$j}}</button>
+	<body>
 
-								{{-- @todo  $ptime  ['price'] 价格['seatinfo']  获取售卖出的 座位数组 遍历后输出 --}}
-							@elseif ($value[$j]=='s')
+	<div id="main">
+		<h2 class="top_title">{{$ctit->shopname}}</h2>
+		<div class="demo">
 
-								<button cowsnumber="{{$i+1}}"  issale = "{{$value[$j]}}" disabled style="color:orangered;" value="{{$value[$j]}}">{{$j}}</button>
+			<center>
+				<div id="seat-map">
+					<div class="front">屏幕中央</div>
+					<div id="newseatmap">
+						@foreach ($layout as $key=>$value)
+						@endforeach
+						@for ($i = 0; $i <=$key; $i++)
+							<ul value="{{($i)}}">
+								@php
+								{{-- @todo 处理一个数组将座位信息删除  显示真实的座位信息 --}}
+								@endphp
+								@for($j=0;$j<count($value);$j++)
+									@if($value[$j]=='a')
+										<button class="seatbutton" style="background-color:#f0f0f0" issale = "{{$value[$j]}}" cowsnumber="{{$i+1}}" value="">{{$j+1}}</button>
+									@elseif ($value[$j]=='_')
+										<button style="visibility:hidden" value="{{$value[$j]}}">{{$j}}</button>
 
-							@endif
+										{{-- @todo  $ptime  ['price'] 价格['seatinfo']  获取售卖出的 座位数组 遍历后输出 --}}
+									@elseif ($value[$j]=='s')
+
+										<button cowsnumber="{{$i+1}}"  issale = "{{$value[$j]}}" disabled style="color:orangered;" value="{{$value[$j]}}">{{$j}}</button>
+
+									@endif
+								@endfor
+
+							</ul>
 						@endfor
-
-					</ul>
-				@endfor
 
 
 					</div>
@@ -91,35 +95,37 @@ span.seatCharts-legendDescription {margin-left: 5px;line-height: 30px;}
 			</div>
 			<div style="clear:both"></div>
 		</div>
+
 		<br/>
 	</div>
+	<script src="{{asset('home/js/jquery.min.js')}}"></script>
+	<script type="text/javascript" src="{{asset('home/js/jquery.seat-charts.min.js')}}"></script>
+	<script type="text/javascript">
 
-
-<script src="{{asset('home/js/jquery.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('home/js/jquery.seat-charts.min.js')}}"></script>
-<script type="text/javascript">
-
-	var arr =[];
-	$('.seatbutton').toggle(function(){
-		if(arr.length<=3) {
-			var cowsnumber = parseInt(($(this).attr('cowsnumber')));
-			var seat = parseInt(($(this).html()));
-			if (arr.length == 0) {
-				arr.push([cowsnumber, seat]);
-				console.log('aaa', arr);
-			} else {
-				if (!isInArray(arr, cowsnumber, seat)) {
-					arr.push([cowsnumber, seat]);
-					console.log('后来的数组', arr);
+		var arr =[];
+		$('.seatbutton').toggle(function(){
+			if(arr.length<=3){
+				var cowsnumber = parseInt(($(this).attr('cowsnumber')));
+				var seat =  parseInt(($(this).html()));
+				if(arr.length==0){
+					arr.push([cowsnumber,seat]);
+					console.log('aaa',arr);
+				}else {
+					if(!isInArray(arr,cowsnumber,seat)){
+						arr.push([cowsnumber,seat]);
+						console.log('后来的数组',arr);
+					}
 
 				}
-				if (arr.length == 4) {
-					alert('只允许选四张');
-				}
+				$('#counter').html(arr.length);
+				$('#total').html(arr.length*{{intval($ptime['price'])}});
+				$('#selected-seats').append("<span id="+cowsnumber+seat+">"+cowsnumber+"排"+seat+"座"+"</span>");
+				$(this).css('background','green');//('display','null');
 			}
-		}
-
-	},function(){
+			if(arr.length==4){
+				alert('只允许选四张');	
+			}
+		}, function(){
 			//元素隐藏 代码④
 			var cowsnumber = ($(this).attr('cowsnumber'));
 			var seat =  ($(this).html());
@@ -172,8 +178,10 @@ span.seatCharts-legendDescription {margin-left: 5px;line-height: 30px;}
 
 				});
 				$(this).disabled;
-				}
+			}
 		}
-</script>
+
+	</script>
 
 @endsection
+
